cf51fe2bb355c672bcb852e12c8940102e54dc73
hanwei <hancenter808@outlook.com>
2021-04-29 03:01:57 +0800

    代码提交

---
+++ src/layouts/Default.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="layout">
+    <header class="header sticky" data-v-7e33895e="">
+      <div class="container" data-v-7e33895e="">
+        <div class="left" data-v-7e33895e="">
+          <a
+            href="/"
+            aria-current="page"
+            class="home-link active--exact active"
+            data-v-7e33895e=""
+            ><img
+              src="/img/logo.332701b7.svg"
+              alt="Nichlas W. Andersen"
+              class="logo"
+              data-v-7e33895e=""
+          /></a>
+        </div>
+        <nav class="nav right" data-v-7e33895e="">
+          <a href="/journal" class="nav__link" data-v-7e33895e="">Journal</a
+          ><a href="/contact" class="nav__link" data-v-7e33895e="">Say Hi!</a>
+        </nav>
+      </div>
+    </header>
+    <slot />
+    <footer class="footer" data-v-07acb5f8="">
+      <div class="container" data-v-07acb5f8="">
+        <span data-v-07acb5f8="">Copyright © 2021</span>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style scoped>
+@import "index.css";
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,161 @@
+<template>
+  <Layout>
+    <div class="container">
+      <div class="hero">
+        <h1 class="hero-title">Simplicity. Aesthetics. Value.</h1>
+        <h2 class="hero-subtitle">
+          Hi there, I'm an independent Digital Designer &amp; Art Director
+          focused on digital design for brands that like to have fun.
+        </h2>
+      </div>
+      <div class="projects" data-v-50cbff3e="">
+        <div class="project" data-v-50cbff3e="">
+          <a
+            href="/projects/chelsea-landmark/"
+            class="project-link"
+            data-v-50cbff3e=""
+            ><img
+              alt="Banana"
+              src="/img/mike-dorner-173502-unsplash.1c9e94c.16e7958f24c375d7bd7b08073b4ce000.jpg"
+              width="2560"
+              data-src="/img/mike-dorner-173502-unsplash.c0dad42.16e7958f24c375d7bd7b08073b4ce000.jpg"
+              data-srcset="/img/mike-dorner-173502-unsplash.b6e36c6.16e7958f24c375d7bd7b08073b4ce000.jpg 480w, /img/mike-dorner-173502-unsplash.1c9e94c.16e7958f24c375d7bd7b08073b4ce000.jpg 1024w, /img/mike-dorner-173502-unsplash.f11e5e1.16e7958f24c375d7bd7b08073b4ce000.jpg 1920w, /img/mike-dorner-173502-unsplash.c0dad42.16e7958f24c375d7bd7b08073b4ce000.jpg 2560w"
+              data-sizes="(max-width: 2560px) 100vw, 2560px"
+              class="thumbnail g-image g-image--lazy g-image--loaded"
+              data-v-50cbff3e=""
+              srcset="
+                /img/mike-dorner-173502-unsplash.b6e36c6.16e7958f24c375d7bd7b08073b4ce000.jpg  480w,
+                /img/mike-dorner-173502-unsplash.1c9e94c.16e7958f24c375d7bd7b08073b4ce000.jpg 1024w,
+                /img/mike-dorner-173502-unsplash.f11e5e1.16e7958f24c375d7bd7b08073b4ce000.jpg 1920w,
+                /img/mike-dorner-173502-unsplash.c0dad42.16e7958f24c375d7bd7b08073b4ce000.jpg 2560w
+              "
+              sizes="(max-width: 2560px) 100vw, 2560px"
+            /><noscript data-v-50cbff3e=""
+              ><img
+                src="/img/mike-dorner-173502-unsplash.c0dad42.16e7958f24c375d7bd7b08073b4ce000.jpg"
+                class="thumbnail g-image g-image--loaded"
+                width="2560"
+                alt="Banana"
+            /></noscript>
+            <h3 class="project-title" data-v-50cbff3e="">Banana</h3>
+            <div class="categories" data-v-50cbff3e="">
+              <span class="category" data-v-50cbff3e="">photography</span
+              ><span class="category" data-v-50cbff3e="">pink</span>
+            </div></a
+          >
+        </div>
+        <div class="project" data-v-50cbff3e="">
+          <a href="/projects/sunk/" class="project-link" data-v-50cbff3e=""
+            ><img
+              alt="Pineapple"
+              src="/img/cody-davis-253925-unsplash.c0dad42.ce547449449f5efa1a8b8f02c7abb04d.jpg"
+              width="2560"
+              data-src="/img/cody-davis-253925-unsplash.c0dad42.ce547449449f5efa1a8b8f02c7abb04d.jpg"
+              data-srcset="/img/cody-davis-253925-unsplash.b6e36c6.ce547449449f5efa1a8b8f02c7abb04d.jpg 480w, /img/cody-davis-253925-unsplash.1c9e94c.ce547449449f5efa1a8b8f02c7abb04d.jpg 1024w, /img/cody-davis-253925-unsplash.f11e5e1.ce547449449f5efa1a8b8f02c7abb04d.jpg 1920w, /img/cody-davis-253925-unsplash.c0dad42.ce547449449f5efa1a8b8f02c7abb04d.jpg 2560w"
+              data-sizes="(max-width: 2560px) 100vw, 2560px"
+              class="thumbnail g-image g-image--lazy g-image--loaded"
+              data-v-50cbff3e=""
+              srcset="
+                /img/cody-davis-253925-unsplash.b6e36c6.ce547449449f5efa1a8b8f02c7abb04d.jpg  480w,
+                /img/cody-davis-253925-unsplash.1c9e94c.ce547449449f5efa1a8b8f02c7abb04d.jpg 1024w,
+                /img/cody-davis-253925-unsplash.f11e5e1.ce547449449f5efa1a8b8f02c7abb04d.jpg 1920w,
+                /img/cody-davis-253925-unsplash.c0dad42.ce547449449f5efa1a8b8f02c7abb04d.jpg 2560w
+              "
+              sizes="(max-width: 2560px) 100vw, 2560px"
+            /><noscript data-v-50cbff3e=""
+              ><img
+                src="/img/cody-davis-253925-unsplash.c0dad42.ce547449449f5efa1a8b8f02c7abb04d.jpg"
+                class="thumbnail g-image g-image--loaded"
+                width="2560"
+                alt="Pineapple"
+            /></noscript>
+            <h3 class="project-title" data-v-50cbff3e="">Pineapple</h3>
+            <div class="categories" data-v-50cbff3e="">
+              <span class="category" data-v-50cbff3e="">photography</span
+              ><span class="category" data-v-50cbff3e="">blue</span>
+            </div></a
+          >
+        </div>
+        <div class="project" data-v-50cbff3e="">
+          <a href="/projects/3d-graff/" class="project-link" data-v-50cbff3e=""
+            ><img
+              alt="Ice Cream"
+              src="/img/ian-dooley-281897-unsplash.c0dad42.4ee4f858cbb8392c2cc4019598c07dcd.jpg"
+              width="2560"
+              data-src="/img/ian-dooley-281897-unsplash.c0dad42.4ee4f858cbb8392c2cc4019598c07dcd.jpg"
+              data-srcset="/img/ian-dooley-281897-unsplash.b6e36c6.4ee4f858cbb8392c2cc4019598c07dcd.jpg 480w, /img/ian-dooley-281897-unsplash.1c9e94c.4ee4f858cbb8392c2cc4019598c07dcd.jpg 1024w, /img/ian-dooley-281897-unsplash.f11e5e1.4ee4f858cbb8392c2cc4019598c07dcd.jpg 1920w, /img/ian-dooley-281897-unsplash.c0dad42.4ee4f858cbb8392c2cc4019598c07dcd.jpg 2560w"
+              data-sizes="(max-width: 2560px) 100vw, 2560px"
+              class="thumbnail g-image g-image--lazy g-image--loaded"
+              data-v-50cbff3e=""
+              srcset="
+                /img/ian-dooley-281897-unsplash.b6e36c6.4ee4f858cbb8392c2cc4019598c07dcd.jpg  480w,
+                /img/ian-dooley-281897-unsplash.1c9e94c.4ee4f858cbb8392c2cc4019598c07dcd.jpg 1024w,
+                /img/ian-dooley-281897-unsplash.f11e5e1.4ee4f858cbb8392c2cc4019598c07dcd.jpg 1920w,
+                /img/ian-dooley-281897-unsplash.c0dad42.4ee4f858cbb8392c2cc4019598c07dcd.jpg 2560w
+              "
+              sizes="(max-width: 2560px) 100vw, 2560px"
+            /><noscript data-v-50cbff3e=""
+              ><img
+                src="/img/ian-dooley-281897-unsplash.c0dad42.4ee4f858cbb8392c2cc4019598c07dcd.jpg"
+                class="thumbnail g-image g-image--loaded"
+                width="2560"
+                alt="Ice Cream"
+            /></noscript>
+            <h3 class="project-title" data-v-50cbff3e="">Ice Cream</h3>
+            <div class="categories" data-v-50cbff3e="">
+              <span class="category" data-v-50cbff3e="">photography</span
+              ><span class="category" data-v-50cbff3e="">yellow</span>
+            </div></a
+          >
+        </div>
+        <div class="project" data-v-50cbff3e="">
+          <a
+            href="/projects/ios-concept/"
+            class="project-link"
+            data-v-50cbff3e=""
+            ><img
+              alt="Porta400"
+              src="/img/markus-spiske-516263-unsplash.c0dad42.5fc4651133067e67284df56f3d81be2d.jpg"
+              width="2560"
+              data-src="/img/markus-spiske-516263-unsplash.c0dad42.5fc4651133067e67284df56f3d81be2d.jpg"
+              data-srcset="/img/markus-spiske-516263-unsplash.b6e36c6.5fc4651133067e67284df56f3d81be2d.jpg 480w, /img/markus-spiske-516263-unsplash.1c9e94c.5fc4651133067e67284df56f3d81be2d.jpg 1024w, /img/markus-spiske-516263-unsplash.f11e5e1.5fc4651133067e67284df56f3d81be2d.jpg 1920w, /img/markus-spiske-516263-unsplash.c0dad42.5fc4651133067e67284df56f3d81be2d.jpg 2560w"
+              data-sizes="(max-width: 2560px) 100vw, 2560px"
+              class="thumbnail g-image g-image--lazy g-image--loaded"
+              data-v-50cbff3e=""
+              srcset="
+                /img/markus-spiske-516263-unsplash.b6e36c6.5fc4651133067e67284df56f3d81be2d.jpg  480w,
+                /img/markus-spiske-516263-unsplash.1c9e94c.5fc4651133067e67284df56f3d81be2d.jpg 1024w,
+                /img/markus-spiske-516263-unsplash.f11e5e1.5fc4651133067e67284df56f3d81be2d.jpg 1920w,
+                /img/markus-spiske-516263-unsplash.c0dad42.5fc4651133067e67284df56f3d81be2d.jpg 2560w
+              "
+              sizes="(max-width: 2560px) 100vw, 2560px"
+            /><noscript data-v-50cbff3e=""
+              ><img
+                src="/img/markus-spiske-516263-unsplash.c0dad42.5fc4651133067e67284df56f3d81be2d.jpg"
+                class="thumbnail g-image g-image--loaded"
+                width="2560"
+                alt="Porta400"
+            /></noscript>
+            <h3 class="project-title" data-v-50cbff3e="">Porta400</h3>
+            <div class="categories" data-v-50cbff3e="">
+              <span class="category" data-v-50cbff3e="">photography</span
+              ><span class="category" data-v-50cbff3e="">yellow</span>
+            </div></a
+          >
+        </div>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello, world!",
+  },
+};
+</script>
+
+<style scoped>
+@import "index.css";
+</style>
